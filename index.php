4c2e65c8040dcbc28b6eabfc477497d26602a6ee
Jiah Kwon <itisgia@gmail.com>
2018-10-12 14:47:16 +1300

    buying section added

--- index.php
+++ index.php
@@ -5,24 +5,26 @@
         <title>Pegasus Books</title>
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <!-- <link href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,600,800" rel="stylesheet"> -->
-		<link href="https://fonts.googleapis.com/css?family=Poiret+One|Cormorant+Garamond|Poppins:300,400,500,600,800" rel="stylesheet">
+		<link href="https://fonts.googleapis.com/css?family=Poiret+One|Cormorant+Garamond|Poppins:300,400,500,600,700,800" rel="stylesheet">
 		<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.3.1/css/all.css" integrity="sha384-mzrmE5qonljUremFsqc01SB46JvROS7bZs3IO2EmfFsd15uHvIt+Y8vEf7N7fWAU" crossorigin="anonymous">
         <link rel="stylesheet" href="css/style.css">
     </head>
     <body>
-		<div class="name">
-			<a href="index">Pegasus Bookshop <i class="fas fa-book-open"></i></a>
-		</div>
-		<div class="mobileNavBar">
-			<div class="menu">
-				<span></span>
-				<span></span>
-				<span></span>
+		<div class="header">
+			<div class="name">
+				<a href="index.php">Pegasus Bookshop <i class="fas fa-book-open"></i></a>
 			</div>
-			<div class=" menu icons">
-				<i class="fas fa-search"></i>
-				<i class="far fa-user"></i>
-				<i class="fas fa-shopping-cart"></i>
+			<div class="mobileNavBar">
+				<div class="menu">
+					<span></span>
+					<span></span>
+					<span></span>
+				</div>
+				<div class=" menu icons">
+					<i class="fas fa-search"></i>
+					<i class="far fa-user"></i>
+					<i class="fas fa-shopping-cart"></i>
+				</div>
 			</div>
 		</div>
 		<div class="entry">
@@ -34,13 +36,50 @@
 		<div class="description">
 			<img src="img/enterance.jpg" alt="books">
 		</div>
-		<div class="anoutPegasus">
+		<div class="anout Pegasus">
 			<h2>About Pegasus Bookshop</h2>
 			<p>Since 1979, Pegasus Bookshop has brought pleasure to book-lovers in Wellington. As a small independent bookseller we aim to stock quality books at reasonable prices.</p>
 		</div>
-		<div class="books">
-			<img src="img/event.png" alt="">
+		<div class="booksWrapper">
+			<img class="speaker" src="img/event.png" alt="speaker icon">
 			<p class="head">Forthcoming Events & Book Clubs</p>
+			<div class="booksAndEvents">
+				<a href="#"><img class="poster" width="600" height="300"src="img/poster.jpg" alt="event poster"></a>
+				<div class="eventsInfo">
+					<p class="whatIsIt">Events : October 23, 2018</p>
+					<p class="nameOfEvent">Literary Landscapes: A Panel Discussion with John Sutherland – 23rd October</p>
+					<a href="#">More Information and Booking <i class="fas fa-angle-right"></i></a>
+				</div>
+			</div>
+			<div class="booksAndEvents">
+				<a href="#"><img class="poster" width="600" height="300"src="img/estanica.jpg" alt="event poster"></a>
+				<div class="eventsInfo">
+					<p class="whatIsIt">Events : November 13, 2018</p>
+					<p class="nameOfEvent">The Estanica: Martín Cullen in Conversation with Elisa Segrave – 13th November</p>
+					<a href="#">More Information and Booking <i class="fas fa-angle-right"></i></a>
+				</div>
+			</div>
+			<div class="booksAndEvents">
+				<a href="#"><img class="poster" width="600" height="300"src="img/hanna.jpg" alt="event poster"></a>
+				<div class="eventsInfo">
+					<p class="whatIsIt">Events : November 08, 2018</p>
+					<p class="nameOfEvent">Three Poems: Hannah Sullivan in Conversation with Adam Phillips – 8th November </p>
+					<a href="#">More Information and Booking <i class="fas fa-angle-right"></i></a>
+				</div>
+			</div>
+			<div class="seeMore">
+				<p>– See More –</p>
+			</div>
+
+		</div>
+		<div class="buyWrapper">
+			<img src="img/tobuy.png" alt="icon image">
+			<p class="toBuy">TO BUY</p>
+
+			<div class="anout">
+				<h2>Books & Other Neccessities</h2>
+				<p>We are pleased to offer a small selection of books and other necessities on our website.  If there is something in particular you are looking for, please <a class="contactUs">contact us directly</a></p>
+			</div>
 
 		</div>
 
